7aabbf72d9e86e8be373eb213fe37ceca42f12d3
Yixin Gao <41952501+Amygyx@users.noreply.github.com>
2026-08-19 11:36:21 +0800

    docs: use learned image compression terminology

--- scripts/build_readme.py
+++ scripts/build_readme.py
@@ -333,14 +333,14 @@ def _project_cell(project_url: str | None) -> str:
 
 def render(data: dict, papers: list[dict]) -> str:
     lines = [
-        "# Papers for Neural Image Compression",
-        "**Purpose:** We aim to provide a summary of neural image compression. More papers will be summarized.",
+        "# Papers for Learned Image Compression",
+        "**Purpose:** We aim to provide a summary of learned image compression. More papers will be summarized.",
         "",
         "University of Science and Technology of China (USTC), "
         "[Intelligent Media Computing Lab](https://faculty.ustc.edu.cn/chenzhibo).",
         "",
         "**📌 About new works.** If you want to incorporate your studies (e.g., the link of paper or project) "
-        "on neural image compression in this repository. Welcome to raise an issue or email us. We will "
+        "on learned image compression in this repository. Welcome to raise an issue or email us. We will "
         "incorporate it into this repository and our survey report as soon as possible.",
         "",
         "## Contents",
